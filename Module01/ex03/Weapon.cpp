88343ecf587378632c192fbf5447df0266163938
avan-bre <ambervanbree@gmail.com>
2022-04-12 18:28:12 +0200

    made Weapon class

---
+++ Module01/ex03/Weapon.cpp
@@ -0,0 +1,18 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : _type(type = "Default"){
+	std::cout << "A weapon is created" << std::endl;
+}
+
+Weapon::Weapon(void){
+	std::cout << "A weapon has been destroyed" << std::endl;
+}
+
+const std::string &Weapon::getType(void){
+	return _type;
+}
+
+void Weapon::setType(std::string newType){
+	this->_type = newType;
+	return ;
+}
---
+++ Module01/ex03/Weapon.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Weapon{
+	private:
+		std::string	_type;
+
+	public:
+		Weapon(std::string type);
+		~Weapon(void);
+
+		const std::string &getType(void);
+		void setType(std::string newType);
+};
