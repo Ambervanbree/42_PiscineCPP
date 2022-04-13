4c712de0c3fe27a0bd574106ea30490ac4ccf560
avan-bre <avan-bre@student.42.fr>
2022-04-13 14:38:09 +0200

    rearranged ex04

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 10:32:10 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/04/13 14:05:23 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/04/13 14:37:52 by avan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,36 @@
 #include <fstream>
 #include <string>
 
-int openWrite(std::ofstream	*ofs, char const *filename){
-	(*ofs).open(filename, std::ofstream::out);
-	if ((*ofs).is_open()){
-		return (1);
-	}
-	else {
-		std::cout << "Error! Out file could not be opened." << std::endl;
-		return (0);
-	}	
-}
-
-int	openRead(std::ifstream *ifs, char const *file){
-	(*ifs).open(file, std::ifstream::in);
-	if ((*ifs).is_open()){
-		return (1);
-	}
-	else {
-		std::cout << "Error! In file could not be opened." << std::endl;
-		return (0);
-	}
-}
-
 int main(int argc, char *argv[]){
-	std::ifstream	ifs;
-	std::ofstream	ofs;
 	if (argc != 4){
 		std::cout << "Error! Expected usage: ./replace < infile > < string1 > < string2 >" << std::endl;
 		return (1);
 	}
-	if (!openRead(&ifs, argv[1]))
-		return (1);
-	if (!openWrite(&ofs, "outfile")){
-		ifs.close();
-		return (1);
+	std::ifstream	ifs(argv[1]);
+	if (ifs.is_open()){
+		std::string 	string = argv[1];
+		string.append(".replace");
+		std::ofstream	ofs(string.c_str());
+		if (ofs.is_open()){
+			std::string		line;
+			while (getline(ifs, line)){
+				if (!line.compare(argv[2]))
+					ofs << argv[3] << std::endl;
+				else 
+					ofs << line << std::endl;
+			}
+		}
+		else {
+			std::cout << "Error! Out file could not be opened." << std::endl;
+			ifs.close();
+			return (1);
+		}	
+		ofs.close();
 	}
-	std::string	line;
-	while (getline(ifs, line)){
-		if (!line.compare(argv[2]))
-			ofs << argv[3] << std::endl;
-		else 
-			ofs << line << std::endl;
+	else {
+		std::cout << "Error! In file could not be opened." << std::endl;
+		return (1);
 	}
 	ifs.close();
-	ofs.close();
+	return (0);
 }
