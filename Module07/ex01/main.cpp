e43b19e353d73e4190b384a956ba03b27e68ad33
Amber <78368988+Ambervanbree@users.noreply.github.com>
2022-06-05 16:43:20 +0200

    ok

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -17,5 +17,5 @@ int     main(void){
     int         array[4] = {1, 2, 3, 4};
     std::string  stringArray[6] = {"yay", "this", "module", "is", "almost", "over"};
     iter(array, 4, printItem);
-    iter(array, 6, spellBackwards);
+    iter(array, 6, spellBackwards<char>);
 }
