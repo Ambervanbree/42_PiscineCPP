6d72f01a0c091bb7bfdf181b536e108c9b192eb4
Amber <78368988+Ambervanbree@users.noreply.github.com>
2022-06-05 16:12:58 +0200

    ok

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -1,11 +1,13 @@
+#include "iter.hpp"
+
 void    plusOne(int *n){
     (*n)++;
     return ;
 }
 
 int     main(void){
-    int array = {1, 2, 3, 4};
-    for (int i = 0; i < 4, i++){
+    int array[4] = {1, 2, 3, 4};
+    for (int i = 0; i < 4; i++){
         std::cout << array[i] << std::endl;
     }
     iter(array, 4, plusOne(array));
